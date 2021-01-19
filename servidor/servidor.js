04e3e64e5232d94a95b1b95de8e70483f445cde5
Yaser Haddad <mohamedyaserhaddad@gmail.com>
2021-01-19 21:49:15 +0100

    Creando servidor con Sockets y añadiendo carpeta cliente(React)

--- servidor/servidor.js
+++ servidor/servidor.js
@@ -1,2 +1,16 @@
 const express = require("express");
-const http = require("http");
+const http = require("http"); //PARA LA CONEXION CON SOCKETS
+
+const app = express(); // FUNCION PARA EL SERVIDOR
+const servidor = http.createServer(app); // cREAMOS NUESTRO SERVIDOR
+
+const socketio = require("socket.io"); // LLAMAMOS A SOCKET
+const io = socketio(servidor); // Sirve para hacer a cualquier app con sockets
+
+io.on("conexion", (socket) => {
+  socket.on("conectado", () => {
+    console.log("usuario conectado");
+  });
+}); //para establecer la conexion
+
+servidor.listen(3000, () => console.log("Servidor inicializado")); // npm run dev : iniciar el servidor
